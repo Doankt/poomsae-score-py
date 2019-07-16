728c81ef4441719da2fa2e161f7dd8e01ea08ba6
Kenneth Doan <doankt@uci.edu>
2019-07-16 13:18:24 -0700

    __init__ can now generate scorelist

--- poomsae-score-py/fullscore.py
+++ poomsae-score-py/fullscore.py
@@ -7,13 +7,15 @@ def _valid_num_judges(num_judges: int):
     return 0 < num_judges < MAX_JUDGES
 
 class FullScore:
-    def __init__(self, num_judges: int = 5):
+    def __init__(self, num_judges: int = 5, score_list=[]):
         if _valid_num_judges(num_judges): self.num_judges = num_judges
         else: raise ValueError("num_judges= {} is out of bounds".format(num_judges))
-        self.score_list = []
-        
-        for i in range(num_judges):
-            self.score_list.append(None)
+
+        if(score_list):
+            self.score_list = [score.Score(s[0], s[1]) for s in score_list]
+        else:
+            for i in range(num_judges):
+                self.score_list.append(None)
         
         print(self.score_list)
     
@@ -76,12 +78,9 @@ class FullScore:
         if num_valid == 0:  return 0.0
         return round(ret / num_valid, 3)
     
-    def __str__(self):
-        ret = '| '
-        for s in self.score_list:
-            ret += str(s) + ' | '
-        
-        return ret
+    def __repr__(self):
+        ret = 'FullScore(total_avg={})'
+        return ret.format(self.total_avg())
                 
     
 def gen_random_fullscore(num_judges):
