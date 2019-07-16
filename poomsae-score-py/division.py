b445ff4adba5994b7d3fc4127fbcdebd44fb9e73
Kenneth Doan <doankt@uci.edu>
2019-07-16 04:15:46 -0700

    Changed project structure to MVC

--- poomsae-score-py/division.py
+++ poomsae-score-py/division.py
@@ -1,9 +1,14 @@
+from collections import OrderedDict
+
+import random
 import fullscore
+import csv
+
 
 class Division:
     def __init__(self, d_name):
         self.division_name = d_name
-        self.full_score_list = dict()
+        self.full_score_list = OrderedDict()
         
     def add_competitor(self, competitor):
         self.full_score_list[competitor] = None
@@ -12,5 +17,6 @@ class Division:
         if competitor in self.full_score_list:
             print('g')
             self.full_score_list[competitor] = fs
-        
-        
+
+    def gen_random_order(self):
+        return random.sample(self.full_score_list.keys(), k=len(self.full_score_list))
--- poomsae-score-py/mainapp.py
+++ poomsae-score-py/mainapp.py
@@ -1,29 +1,43 @@
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+import tkinter.ttk as ttk
+
+from collections import namedtuple
+
 import score
 import fullscore
 import competitor
 import division
+from audience_view import AudienceTopLevel
+from admin_view import AdminTopLevel
 
-from tkinter import *
+# Controller
+class MainApp(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.withdraw()
+        self.admin_view = AdminTopLevel(self)
+        self._define_buttons()
+
+    def _define_buttons(self):
+        # Admin View
+        admin_frame = self.admin_view.admin_frame
+
+        admin_frame.aud_view_button.bind('<Button-1>', self._show_audience_view)
+        admin_frame.open_division_button.bind('<Button-1>', self._make_divison)
+
+    def _make_divison(self, event):
+        file = askopenfilename(initialdir='.',
+                               filetypes=[('Poomsae Score Divison', '.psdiv')])
+        print(file)
+
+    def _show_audience_view(self, event):
+        self.audience_view = AudienceTopLevel(self)
+
+    def _randomscore(self, event):
+        print(event.widget)
+        event.widget.config(text=score.gen_random_score())
 
 if __name__ == '__main__':
-    root = Tk()
-    
-    score_frame = Frame(root, width=800, height=600)
-    
-    judge_0 = Label(score_frame, text='0.00', fg='yellow', bg='black')
-    judge_1 = Label(score_frame, text='0.00', fg='yellow', bg='black')
-    judge_2 = Label(score_frame, text='0.00', fg='yellow', bg='black')
-    judge_3 = Label(score_frame, text='0.00', fg='yellow', bg='black')
-    judge_4 = Label(score_frame, text='0.00', fg='yellow', bg='black')
-    
-    judge_0.pack(side=LEFT)
-    judge_1.pack(side=LEFT)
-    judge_2.pack(side=LEFT)
-    judge_3.pack(side=LEFT)
-    judge_4.pack(side=LEFT)
-
-    
-    score_frame.pack()
-    
+    root = MainApp()
     root.mainloop()
-    
