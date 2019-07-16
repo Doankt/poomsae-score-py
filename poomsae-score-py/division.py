55e46c5764a5de80adc4b86ab39a39a1c6707cb5
Kenneth Doan <doankt@uci.edu>
2019-07-16 13:17:58 -0700

    Added ring_number member

--- poomsae-score-py/division.py
+++ poomsae-score-py/division.py
@@ -2,20 +2,21 @@ from collections import OrderedDict
 
 import random
 import fullscore
-import csv
+import json
+
 
 
 class Division:
-    def __init__(self, d_name):
+    def __init__(self, d_name, ring_number=1):
         self.division_name = d_name
+        self.ring_number = ring_number
         self.full_score_list = OrderedDict()
-        
+
     def add_competitor(self, competitor):
         self.full_score_list[competitor] = None
-    
+
     def set_score(self, competitor, fs):
         if competitor in self.full_score_list:
-            print('g')
             self.full_score_list[competitor] = fs
 
     def gen_random_order(self):
