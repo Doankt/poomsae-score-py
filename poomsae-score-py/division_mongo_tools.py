e40589ad6d8af9b73f15ea5b34b4b9b34aeee5c1
Kenneth Doan <doankt@uci.edu>
2019-07-17 03:31:08 -0700

    Addded skeleton for creating user

--- poomsae-score-py/division_mongo_tools.py
+++ poomsae-score-py/division_mongo_tools.py
@@ -1,12 +1,17 @@
 import json
-
+from bson.objectid import ObjectId
 import pymongo
-import bson
+from passlib.hash import argon2
+
 from division import Division
 from competitor import Competitor
 from score import Score
 from fullscore import FullScore
 
+USER_MNGR_USER = 'userManager'
+USER_MNGR_PASSWD = 'mg1ImFbYgdYRi9gt'
+
+# TEMP_API_KEY ='61762a77-af25-4b2c-9cc1-0dc2df4e8ee8'
 
 def psdiv_load(file):
     with open(file, 'r') as json_file:
@@ -45,10 +50,47 @@ def upload_division(division):
     enco = _psdiv_encode(division)
 
     client = pymongo.MongoClient(
-        "mongodb+srv://doankt:Buy1get1free@poomsaescore-rpi4g.mongodb.net/test?retryWrites=true&w=majority")
+        "mongodb+srv://{username}:{password}@poomsaescore-rpi4g.mongodb.net/test?retryWrites=true&w=majority".format(
+            username='doankt',
+            password='Buy1get1free'
+        ))
     working_db = client['tournaments']
     working_collection = working_db['Test Tournament']
 
     res = working_collection.insert_one(enco)
     print(res.inserted_id)
-    return bson.objectid.ObjectId(res.inserted_id)
+    return ObjectId(res.inserted_id)
+
+
+#
+# def create_account_test():
+#     client = pymongo.MongoClient(
+#         "mongodb+srv://{username}:{password}@poomsaescore-rpi4g.mongodb.net/test?retryWrites=true&w=majority".format(
+#             username = 'doankt',
+#             password = 'Buy1get1free'
+#     ))
+#     working_db = client['tournaments']
+#     print(working_db)
+#     # working_collection = working_db['Test Tournament']
+#     res = working_db.command('createUser', 'name')
+#     print(res)
+
+# create_account_test()
+def create_user(email, passwd):
+    client = pymongo.MongoClient(
+        "mongodb+srv://{username}:{password}@poomsaescore-rpi4g.mongodb.net/test?retryWrites=true&w=majority".format(
+            username=USER_MNGR_USER,
+            password=USER_MNGR_PASSWD))
+    print(client)
+    users_db = client['users']
+    print(users_db)
+
+    tadmin_collection = users_db['tournamentAdmins']
+    if not tadmin_collection.count_documents({'email': email}):
+        tadmin_collection.insert_one({'email': email,
+                                      'hashed_password': argon2.hash(passwd)})
+        print('Account created')
+    else:
+        print('email taken')
+
+create_user('q', 'q')
