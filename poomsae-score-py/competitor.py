6b2d72b91c6d7cac0c080de3baed1550e63a9310
Kenneth Doan <doankt@uci.edu>
2019-07-17 22:58:47 -0700

    Move modules to packages

--- poomsae-score-py/competitor.py
+++
@@ -1,8 +0,0 @@
-class Competitor:
-    def __init__(self, delim_name=''):
-        self.delim_name = delim_name
-
-    def __repr__(self):
-        ret = 'Competitor({}, {})'
-        sname = self.delim_name.split('~')
-        return ret.format(sname[0], sname[1])
