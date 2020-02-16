ed3750f5fc18a848e4addf4f6f00d730d040bc12
Kenneth Doan <doankt@uci.edu>
2020-02-16 02:41:46 -0800

    Revert "Housekeeping"
    This reverts commit 9b5a58cb5474b503ba8bfba58c145282413b90eb.

--- poomsae-score-py/mainapp.py
+++ poomsae-score-py/mainapp.py
@@ -1,21 +1,16 @@
 import wx
 from collections import namedtuple
-import time
-# import score
-# import fullscore
-# import competitor
-# import division
-from controllers import controller
+
+# import model.score
+# import model.fullscore
+# import model.competitor
+# import model.division
+
 from frames.menu_frame import MenuFrame
 
 
 if __name__ == '__main__':
-    # app = wx.App()
+    app = wx.App()
+    frame = MenuFrame()
     # frame = MenuFrame()
-    # frame = MenuFrame()
-    ard1 = controller.Controller('COM8')
-    ard1.start()
-    while True:
-        print(ard1.score)
-        time.sleep(1)
-    # app.MainLoop()
+    app.MainLoop()
