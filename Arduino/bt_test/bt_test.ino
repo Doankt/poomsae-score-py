e83277eb7a100e0899b0c9f7029eb62f0c2b90ce
Kenneth Doan <doankt@uci.edu>
2020-02-18 16:14:57 -0800

    upload correct version

--- Arduino/bt_test/bt_test.ino
+++ Arduino/bt_test/bt_test.ino
@@ -1,22 +1,39 @@
 #include <PushButton.h>
 #include <SoftwareSerial.h>
 
-int i = 0;
 SoftwareSerial bt(2,3);
 int RESET_BUTTON = 5;
 int BT_POWER = 4;
 int DATA_BUTTON = 6;
+int PING_TIMEOUT = 2000; //in ms
+
+int imode = 0;
 
 float score[2];
+unsigned long last_ping;
+
 
 PushButton reset_pb(RESET_BUTTON);
 PushButton data_pb(DATA_BUTTON);
 
+void interface_write(const char* str){
+  switch(imode){
+    case 0:
+      Serial.write(str);
+      break;
+    case 1:
+      bt.write(str);
+    default:
+      break;
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   bt.begin(9600);
+
+
   pinMode(RESET_BUTTON, INPUT);
   
   pinMode(BT_POWER, OUTPUT);
@@ -40,19 +57,28 @@ void loop() {
     delay(1000);
     digitalWrite(BT_POWER, HIGH);
   }
+  
   if(data_pb.isClicked()){
     char res[5];
+
+    score[0] = random(4*100) / 100.0;
+    score[1] = random(6.0*100) / 100.0;
     
-    Serial.println("Added data");
-    bt.write("<");
+    interface_write("<");
 
     dtostrf(score[0], 2, 1, res);
-    bt.write(res);
+    interface_write(res);
     
-    bt.write(",");
+    interface_write(",");
 
     dtostrf(score[1], 2, 3, res);
-    bt.write(res);
-    bt.write(">");
+    interface_write(res);
+    interface_write(">");
+  }
+
+  
+  if(millis() - last_ping >= PING_TIMEOUT){
+    interface_write("#");
+    last_ping = millis();
   }
 }
