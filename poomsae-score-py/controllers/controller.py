727acbafe08e7291fbf95a3bd8fd32ff1e3e0ae0
Doankt <36061217+Doankt@users.noreply.github.com>
2020-02-26 12:59:43 -0800

    Moved _read_thread to class function

--- poomsae-score-py/controllers/controller.py
+++ poomsae-score-py/controllers/controller.py
@@ -13,64 +13,64 @@ class ControllerDisconnectException(Exception):
     def __str__(self):
         return self.message
 
-def _read_thread(controller):
-    last_ping = time.time()
-    while True:
-        try:
-            if controller.stop_flag:
-                break
-
-            data = controller.ser.read(1)
-            if data == b'<':
-                string = ""
-                while True:
-                    data = controller.ser.read(1)
-                    if data == b'>': break
-                    string += data.decode('utf-8')
-                controller.score = [float(n) for n in string.split(',')]
-            elif data == b'#':
-                print("pinged")
-                last_ping = time.time()
-
-            if((time.time() - last_ping) >= CONTROLLER_TIMEOUT):
-                raise ControllerDisconnectException(controller.ser.port)
-
-        except:
-            print("Controller {} Disconnected".format(controller.ser.port))
-            retry = 0
-
-            port = controller.ser.port
-            baud = controller.ser.baudrate
-            timeo = controller.ser.timeout
-
-            while retry < 5:
-                print("retry:", retry)
-                try:
-                    controller.attempt_reconnect(port, baud, timeo)
-                    break
-                except:
-                    retry += 1
-                    time.sleep(5)
-            if retry >= 3:
-                print("controller failed to reconnect")
-                break
-            print("Controller {} Reconnected".format(controller.ser.port))
-    print("eot")
-
-
 class Controller:
-
-    def __init__(self, port):
+    def __init__(self, port, mainapp, slotnum):
         self.ser = Serial(port, baudrate=9600, timeout=5)
         self.score = []
         self.read_thread = None
-        self.stop_flag = -1
+        self.continue_read_thread = False
+        self.mainapp = mainapp
+        self.slot_num = slotnum
+
+        self.mainapp.controller_list[self.slot_num] = self
+
+    def _read_thread(self):
+        last_ping = time.time()
+        while self.continue_read_thread:
+            try:
+                data = self.ser.read(1)
+                if data == b'<':
+                    string = ""
+                    while True:
+                        data = self.ser.read(1)
+                        if data == b'>': break
+                        string += data.decode('utf-8')
+                    self.score = [float(n) for n in string.split(',')]
+                elif data == b'#':
+                    last_ping = time.time()
+
+                if ((time.time() - last_ping) >= CONTROLLER_TIMEOUT):
+                    raise ControllerDisconnectException(self.ser.port)
+
+            except:
+                print("Controller {} Disconnected".format(self.ser.port))
+                retry = 0
+
+                port = self.ser.port
+                baud = self.ser.baudrate
+                timeo = self.ser.timeout
+
+                while retry < 3:
+                    print("retry:", retry)
+                    try:
+                        self.attempt_reconnect(port, baud, timeo)
+                        if self.ser.read(1) == b'#':
+                            break
+                    finally:
+                        retry += 1
+                        time.sleep(5)
+
+                if retry >= 3:
+                    print("controller failed to reconnect")
+                    break
+                print("Controller {} Reconnected".format(self.ser.port))
+        self.__del__()
 
     def start(self):
         if not self.read_thread:
-            self.read_thread = threading.Thread(target=_read_thread, args=(self,))
+            self.read_thread = threading.Thread(target=self._read_thread)
             self.read_thread.daemon = True
-            self.stop_flag = 0
+            self.continue_read_thread = True
             self.read_thread.start()
 
     def attempt_reconnect(self, p, b, t):
@@ -78,5 +78,11 @@ class Controller:
         self.ser = Serial(p, baudrate=b, timeout=t)
 
     def stop(self):
-        self.stop_flag = 1
+        self.continue_read_thread = False
         self.read_thread = None
+
+    def __del__(self):
+        print("ented")
+        self.continue_read_thread = False
+        self.read_thread = None
+        self.mainapp.controller_list[self.slot_num] = None
